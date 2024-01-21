7c7897dde2b1a0446f8acb567c047f53b4b9e658
Subarna  Bhandari <64469023+subarnabhd@users.noreply.github.com>
2024-01-21 10:02:21 +0000

    test

---
+++ include/nav.php
@@ -0,0 +1,17 @@
+<ul>
+      <li>
+      <a href="index.php"  class="home">Home. </a>
+      </li>
+
+      <li>
+      <a href="import.php" class="import">Import. Purchase.</a>
+      </li>
+
+      <li>
+      <a href="export.php" class="export">Export. Sale.</a>
+      </li>
+      
+      <li>
+      <a href="write.php" class="write">Write to us.</a>
+      </li>       
+</ul>
