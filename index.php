c474457b5510d973ee140bac6983b791f7d94e86
Subarna  Bhandari <64469023+subarnabhd@users.noreply.github.com>
2024-01-21 11:15:39 +0000

    test

--- index.php
+++ index.php
@@ -1,11 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Simple PHP Page on Vercel</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Bhuvaneshwor Traders</title>
+	<link rel="stylesheet" type="text/css" href="Css/Style.css">
+	<link rel="icon" type="image/png" href="image/favicon.ico"/>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
 </head>
-<body>
-<?php
-echo "Hello from PHP on Vercel (hopefully)!";
-?>
-</body>
-</html>
+<body class="home-page">
+	
+<?php include 'include/header.php'; ?>
+
+<!-- Content Start -->
+    
+    <main class="center clearfix">
+    <article>
+        <h1>
+            <span>
+            <ins>Ginger</ins>.
+            <ins>Garlic</ins>.
+            <ins>Apple</ins>.
+            <ins>Onion</ins>.
+            <ins>Satuwa (Herb)</ins>.
+            </span><br>       
+            <strong>purchase. sale. storage. processing. import.<br>export.</strong>
+        </h1>
+
+        <section class="leftcol clearfix">
+
+            <img src="Image/25-years.png" alt="25-years">
+            <div>
+                <p>
+                <b>Bhuvaneshwor Traders</b> is the largest trading company of Agricultural Products that include all available varieties of <b>Ginger</b>, <b>Garlic</b>, <b>Apple</b> and <b>Onion</b>.Besides, we deal also in herbs like <b>Satuwa</b> too.
+                </p>
+                <p>
+                All the international firms we are dealing are one of the largest in their respective countries. Thus, everything we buy/sell or import/export are the best quality products. And we assure, no one ever has to suffer from shortage as we are always able to fulfill requirement for any quantity.
+                </p>
+            </div>
+            
+        </section> 
+
+    </article>
+      <?php include 'include/aside.php'; ?>
+</main>
+
+<!-- Content End -->
+
+<?php include 'include/footer.php'; ?>
+
+
+
+
+
